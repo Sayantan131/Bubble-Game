27eb3d0c09187545d1e5fe897692ccdbdd428623
Sayantan <maitysayantan111@gmail.com>
2023-08-13 16:47:02 +0530

    running

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+
+var timer = 60;
+var score = 0;
+function increaseScore(){
+   score += 10;
+   document.querySelector('#scoreval').textContent = score; 
+}
+
+function getNewHit(){
+    var rn = Math.floor(Math.random()*10);
+    document.querySelector('#hitval').textContent = rn;
+}
+
 function makeBubble(){
 clutter= "";
 for(i=1;i<=185;i++){
@@ -8,5 +21,20 @@ document.querySelector('#pbtm').innerHTML=clutter;
 
 };
 
-makeBubble();
+function runTimer(){
+    var timerint = setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector('#timerval').textContent = timer;  
+        }
+        else{
+            clearInterval(timerint);
+        }
+           
+    },1000);
+}
 
+runTimer();
+makeBubble();
+getNewHit();
+increaseScore();
